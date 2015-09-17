7ef724a834f95af87cb41dd12680e2fcdb1b0a39
Adrian Mouat <adrian@adrianmouat.com>
2015-09-17 11:13:53 +0100

    With caching.

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -1,8 +1,10 @@
 from flask import Flask, Response, request
 import requests
 import hashlib
+import redis
 
 app = Flask(__name__)
+cache = redis.StrictRedis(host='redis', port=6379, db=0)
 salt = "UNIQUE_SALT"
 default_name = 'Joe Bloggs'
 
@@ -16,7 +18,6 @@ def mainpage():
 
     salted_name = salt + name
     name_hash = hashlib.sha256(salted_name.encode()).hexdigest()
-
     header = '<html><head><title>Identidock</title></head><body>'
     body = '''<form method="POST">
               Hello <input type="text" name="name" value="{0}">
@@ -33,8 +34,12 @@ def mainpage():
 @app.route('/monster/<name>')
 def get_identicon(name):
 
-    r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
-    image = r.content
+    image = cache.get(name)
+    if image is None:
+        print ("Cache miss", flush=True)
+        r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
+        image = r.content
+        cache.set(name, image)
 
     return Response(image, mimetype='image/png')
 

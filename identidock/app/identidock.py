ce44d5f2225f3d6c11b41cedd01c1e4ae7483c20
Adrian Mouat <adrian@adrianmouat.com>
2015-09-17 08:58:01 +0100

    First identidock code.

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -1,10 +1,25 @@
 from flask import Flask
+
 app = Flask(__name__)
+default_name = 'Joe Bloggs'
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello Docker!\n'
+def get_identicon():
+
+    name = default_name
+
+    header = '<html><head><title>Identidock</title></head><body>'
+    body = '''<form method="POST">
+              Hello <input type="text" name="name" value="{}">
+              <input type="submit" value="submit">
+              </form>
+              <p>You look like a:
+              <img src="/monster/monster.png"/>
+              '''.format(name)
+    footer = '</body></html>'
+
+    return header + body + footer
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')

e5a3e42f5797d28b16697b2c1b3657f264f5fd97
Adrian Mouat <adrian@adrianmouat.com>
2015-09-17 10:49:40 +0100

    Working with input.

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -1,23 +1,30 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 import requests
+import hashlib
 
 app = Flask(__name__)
+salt = "UNIQUE_SALT"
 default_name = 'Joe Bloggs'
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def mainpage():
 
     name = default_name
+    if request.method == 'POST':
+        name = request.form['name']
+
+    salted_name = salt + name
+    name_hash = hashlib.sha256(salted_name.encode()).hexdigest()
 
     header = '<html><head><title>Identidock</title></head><body>'
     body = '''<form method="POST">
-              Hello <input type="text" name="name" value="{}">
+              Hello <input type="text" name="name" value="{0}">
               <input type="submit" value="submit">
               </form>
               <p>You look like a:
-              <img src="/monster/monster.png"/>
-              '''.format(name)
+              <img src="/monster/{1}"/>
+              '''.format(name, name_hash)
     footer = '</body></html>'
 
     return header + body + footer

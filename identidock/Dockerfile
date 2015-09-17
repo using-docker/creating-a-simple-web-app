c5a4c1f75d47c8b96fb143914492a188be09cecd
Adrian Mouat <adrian@adrianmouat.com>
2015-09-17 10:10:05 +0100

    With dnmonster.

--- identidock/Dockerfile
+++ identidock/Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.4
 
 RUN groupadd -r uwsgi && useradd -r -g uwsgi uwsgi
-RUN pip install Flask==0.10.1 uWSGI==2.0.8
+RUN pip install Flask==0.10.1 uWSGI==2.0.8 requests==2.5.1
 WORKDIR /app
 COPY app /app
 COPY cmd.sh /
--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, Response
+import requests
 
 app = Flask(__name__)
 default_name = 'Joe Bloggs'
 
 
 @app.route('/')
-def get_identicon():
+def mainpage():
 
     name = default_name
 
@@ -20,6 +21,15 @@ def get_identicon():
     footer = '</body></html>'
 
     return header + body + footer
+
+
+@app.route('/monster/<name>')
+def get_identicon(name):
+
+    r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
+    image = r.content
+
+    return Response(image, mimetype='image/png')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
